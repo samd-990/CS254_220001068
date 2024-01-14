b943681f96ec56204a34cc3c76db9ebedc41fb9c
Samyak D <samyakd990@gmail.com>
2024-01-14 23:24:29 +0530

    14 Jan Assignment

--- 14 Jan 23/q1.cpp
+++ 14 Jan 23/q1.cpp
@@ -3,26 +3,32 @@
 using namespace std;
 
 int main() {
-	int n;
-	cin >> n;
-	
-	long long a[n];
-	for (int i = 0; i < n; i++) {
-		cin >> a[i];
-	}
-	
-	long long num1 = max(a[0], a[1]);
-	long long num2 = min(a[0], a[1]);
-	
-	for (int i = 3; i < n; i++) {
-		if (a[i] >= num1) {
-			num2 = num1;
-			num1 = a[i];
+	// ios::sync_with_stdio(0);
+	// cin.tie(0);
+	int t;
+	cin >> t;
+	while (t--) {
+		int n;
+		cin >> n;
+
+		long long a[n];
+		for (int i = 0; i < n; i++) {
+			cin >> a[i];
 		}
-		else if (a[i] >= num2) {
-			num2 = a[i];
+
+		long long num1 = max(a[0] , a[1]);
+		long long num2 = min(a[0] , a[1]);
+
+		for (int i = 2; i < n; i++) {
+			if (a[i] >= num1) {
+				num2 = num1;
+				num1 = a[i];
+			}
+			else if (a[i] >= num2) {
+				num2 = a[i];
+			}
 		}
+
+		cout << num1 + num2 << '\n';
 	}
-	
-	cout << num1 + num2 << '\n';
 }
